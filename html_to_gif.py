0b5f8d2925c572bc57d9551cf3808b98b332b644
Joao Machado <machado000@gmail.com>
2025-07-29 16:15:14 -0300

    Optimize GIF saving

--- html_to_gif.py
+++ html_to_gif.py
@@ -9,8 +9,8 @@ from selenium import webdriver
 
 
 project_path = Path(__file__).parent.resolve()
-source_path = "I:\\Meu Drive\\02 - MATERIAIS\\019_NOVA_CNI\\cni_cebrics_2026_v5"
-destination_path = "I:\\Meu Drive\\02 - MATERIAIS\\019_NOVA_CNI\\cni_cebrics_2026_gifs"
+source_path = "I:\\Meu Drive\\02 - MATERIAIS\\024_NACIONAL RIO_CMRJ_CAMERA APROVA\\nacional_cmrj_cidadania_v4"
+destination_path = "I:\\Meu Drive\\02 - MATERIAIS\\024_NACIONAL RIO_CMRJ_CAMERA APROVA\\nacional_cmrj_cidadania_gifs"
 gifsicle_path = os.path.join(project_path, "gifsicle", "gifsicle.exe")
 
 temp_path = os.path.join(project_path, "temp")
@@ -18,25 +18,43 @@ os.makedirs(temp_path, exist_ok=True)
 
 
 banner_list = [
-    # "banner_1_300x250",
-    # "banner_1_300x600",
-    # "banner_1_728x90",
-    # "banner_1_970x90",
+    "banner_1_300x250",
+    "banner_1_300x600",
+    "banner_1_160x600",
+    "banner_1_728x90",
+    "banner_1_970x90",
+    "banner_1_468x60",
+    "banner_1_970x150",
     "banner_1_970x250",
-    # "banner_1_300x100",
-    # "banner_1_320x100",
-    # "banner_1_320x50",
+    "banner_1_1040x210",
+    "banner_1_300x100",
+    "banner_1_320x150",
+    "banner_1_320x100",
+    "banner_1_336x280",
+    "banner_1_250x250",
+    "banner_1_200x200",
+    "banner_1_320x50",
+    "banner_1_300x50",
 ]
 
 banner_sizes = {
-    "banner_1_300x250": (300, 250, 14),
-    "banner_1_300x600": (300, 600, 14),
-    "banner_1_728x90": (728, 90, 14),
-    "banner_1_970x90": (970, 90, 14),
-    "banner_1_970x250": (970, 250, 14),
-    "banner_1_300x100": (300, 100, 17),
-    "banner_1_320x100": (320, 100, 17),
-    "banner_1_320x50": (320, 50, 13),
+    "banner_1_300x250": (300, 250, 12),
+    "banner_1_300x600": (300, 600, 12),
+    "banner_1_160x600": (160, 600, 12),
+    "banner_1_728x90": (728, 90, 12),
+    "banner_1_970x90": (970, 90, 12),
+    "banner_1_468x60": (468, 60, 12),
+    "banner_1_970x150": (970, 150, 12),
+    "banner_1_970x250": (970, 250, 12),
+    "banner_1_1040x210": (1040, 210, 12),
+    "banner_1_300x100": (300, 100, 12),
+    "banner_1_320x150": (320, 150, 12),
+    "banner_1_320x100": (320, 100, 12),
+    "banner_1_336x280": (336, 280, 12),
+    "banner_1_250x250": (250, 250, 12),
+    "banner_1_200x200": (200, 200, 12),
+    "banner_1_320x50": (320, 50, 12),
+    "banner_1_300x50": (300, 50, 12),
 }
 
 
@@ -48,7 +66,7 @@ for banner in banner_list:
     banner_path = os.path.join(source_path, banner, "index.html")
     width, height, gif_duration_seconds = banner_sizes.get(banner, (300, 250, 15))
 
-    gif_fps = 5
+    gif_fps = 3
     frame_count = gif_duration_seconds * gif_fps
     frame_duration = 1 / gif_fps
 
@@ -66,7 +84,7 @@ for banner in banner_list:
         with Image.open(frame) as img:
             cropped = img.crop((0, 0, width, height))
             cropped = cropped.convert("P", palette=Image.ADAPTIVE, colors=256)
-            cropped.save(frame)
+            cropped.save(frame, optimize=True)
 
         frames.append(imageio.imread(frame))
         frames_paths.append(frame)
@@ -75,17 +93,17 @@ for banner in banner_list:
     gif_path = os.path.join(destination_path, f"{banner}.gif")
     imageio.mimsave(gif_path, frames, format='GIF', duration=int(frame_duration * 2000), loop=0, subrectangles=True)
 
-    # Optimize GIF with pygifsicle
-    optimized_gif_path = os.path.join(destination_path, f"{banner}_optimized.gif")
-    subprocess.run([
-        gifsicle_path,
-        "-O3",
-        "--colors=256",
-        "-b",
-        gif_path,
-        # "-o", optimized_gif_path,
-        # "--size-info"
-    ], check=True)
+    # # Optimize GIF with pygifsicle
+    # optimized_gif_path = os.path.join(destination_path, f"{banner}.gif")
+    # subprocess.run([
+    #     gifsicle_path,
+    #     "-b",
+    #     "--optimize=3",
+    #     "--colors=256",
+    #     "--lossy=100",
+    #     gif_path,
+    #     "-o", optimized_gif_path,
+    # ], check=True)
 
     # Clean up
     for f in frames_paths:
